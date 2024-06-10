c06818b3aeff219bbe564e8e91a374ba40bc5e63
AravDesai <145138457+AravDesai@users.noreply.github.com>
2024-06-10 21:44:53 +0530

    Added config options
    Fan picker may not work - lack desktop to test but will still try to make it work.
    Speedometer fan functionality coming soon

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use eframe::egui;
-use egui::{Color32, Pos2};
+use egui::{Color32, ComboBox, Pos2, Response};
 use nvml_wrapper::enum_wrappers::device::TemperatureSensor;
+use nvml_wrapper::error::NvmlError;
 use nvml_wrapper::Nvml;
 use std::thread;
 use std::time::Duration;
@@ -17,7 +18,7 @@ fn main() -> Result<(), eframe::Error> {
     env_logger::init();
 
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([500.0, 300.0]),
         ..Default::default()
     };
     eframe::run_native(
@@ -40,6 +41,8 @@ struct MyApp {
     //tester: u32,
     nvml: Nvml,
     special_temp: u32,
+    device_indexer: u32,
+    fan_indexer: u32,
 }
 
 impl Default for MyApp {
@@ -59,6 +62,8 @@ impl Default for MyApp {
             //tester: 0
             nvml: Nvml::init().expect("NVML failed to initialize"), // Make this not crash for non nvidia systems/non gpu computers (could be implemented in the update function)
             special_temp: 0,
+            device_indexer: 0,
+            fan_indexer: 0,
         }
     }
 }
@@ -79,7 +84,8 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             //populates gpu_data with gpu information
-            let device_wrapped = self.nvml.device_by_index(0);
+
+            let device_wrapped = self.nvml.device_by_index(self.device_indexer);
             let device = device_wrapped.unwrap();
 
             self.gpu_data = GpuData {
@@ -118,9 +124,10 @@ impl eframe::App for MyApp {
             //Start of ui being built
             ui.heading("GPU Stats");
 
-            //Collapsable menu that shows all the condensed stats
             ui.horizontal(|ui| {
-                ui.collapsing("All stats", |ui| {
+                //Collapsable menu that shows all the condensed stats
+
+                let holder = ui.collapsing("All stats", |ui| {
                     ui.label(&self.gpu_data.name);
                     ui.label(
                         "Memory used: ".to_owned() + &self.gpu_data.memory_used.to_string() + "MB",
@@ -139,6 +146,74 @@ impl eframe::App for MyApp {
                             + &self.special_temp.to_string()
                             + curr_temp_type,
                     );
+                });
+
+                //Config menu
+
+                let holder_rect = egui::Rect {
+                    min: Pos2 {
+                        x: holder.header_response.rect.min.x + 250.0,
+                        y: holder.header_response.rect.min.y,
+                    },
+                    max: holder.header_response.rect.max,
+                };
+
+                let mut devices = vec![];
+
+                if let Ok(i) = self.nvml.device_count() {
+                    devices.push(self.nvml.device_by_index(i - 1).unwrap().name().unwrap());
+                }
+
+                let mut fans = vec![];
+
+                if let Ok(i) = self
+                    .nvml
+                    .device_by_index(self.device_indexer)
+                    .unwrap()
+                    .num_fans()
+                {
+                    fans.push(i);
+                }
+
+                ui.allocate_ui_at_rect(holder_rect, |ui| {
+                    ui.collapsing("Configurations", |ui| {
+                        egui::ComboBox::from_label("GPU Picker")
+                            .selected_text(format!("{devices:?}"))
+                            .show_ui(ui, |ui| {
+                                let mut indexer = 0;
+                                for selectable in devices {
+                                    if ui
+                                        .selectable_value(
+                                            &mut device.name().unwrap(),
+                                            selectable.clone(),
+                                            device.name().unwrap(),
+                                        )
+                                        .clicked()
+                                    {
+                                        self.device_indexer = indexer;
+                                    }
+                                    indexer += 1;
+                                }
+                            });
+
+                        egui::ComboBox::from_label("Fan Picker")
+                            .selected_text(format!("Fan {}", self.fan_indexer))
+                            .show_ui(ui, |ui| {
+                                for (index, fan) in fans.iter().enumerate() {
+                                    let indexer = index as u32;
+                                    if ui
+                                        .selectable_value(
+                                            &mut self.fan_indexer,
+                                            indexer,
+                                            format!("Fan {}", index),
+                                        )
+                                        .clicked()
+                                    {
+                                        self.fan_indexer = indexer;
+                                    }
+                                }
+                            });
+                    });
                 });
             });
 
@@ -170,36 +245,25 @@ impl eframe::App for MyApp {
                 .fill(color_gradient(self.gpu_data.temperature))
                 .animate(self.animate_thermometer_bar);
 
-            //Hovering thermometer
-            // self.animate_thermometer_bar = ui
-            //     .add(thermometer)
-            //     .on_hover_text(
-            //         self.gpu_data.temperature.to_string().as_str().to_owned() + curr_temp_type,
-            //     )
-            //     .hovered();
-
-            let thermometer_rect = ui.add(thermometer).rect;
-
-            //let therm_rect = ;
-
             // Bulb portion of thermometer
 
-            //let rect = egui::Rect::from_min_max(Pos2{x: 80.0, y:80.0}, Pos2{x:280.0, y:280.0});
+            let thermometer_rect = ui.add(thermometer).rect;
 
             ui.allocate_ui_at_rect(thermometer_rect, |ui| {
                 let painter = ui.painter();
                 painter.circle(
                     Pos2 {
-                        x: thermometer_rect.min.x + 15.0,
+                        x: thermometer_rect.min.x + 13.0,
                         y: thermometer_rect.min.y + 8.0,
                     },
-                    19.0,
+                    20.0,
                     color_gradient(self.gpu_data.temperature),
                     egui::Stroke {
                         width: 0.0,
                         color: Color32::from_rgb(255, 255, 255),
                     },
                 );
+
                 let temp_changer = ui
                     .button(self.special_temp.to_string() + curr_temp_type)
                     .on_hover_text("Click to change unit");
@@ -213,6 +277,20 @@ impl eframe::App for MyApp {
             } else {
                 self.special_temp = self.gpu_data.temperature;
             }
+
+            //Fan Speed Info
+            ui.label(
+                "
+            ",
+            ); //more empty space
+
+            let fan_speed = match device.fan_speed(self.fan_indexer) {
+                Ok(speed) => speed,
+                Err(_E) => 0,
+            };
+
+            ui.label("Number of Fans: ".to_owned() + &device.num_fans().unwrap().to_string());
+            ui.label("Fan speed: ".to_owned() + &fan_speed.to_string());
         });
     }
 }
